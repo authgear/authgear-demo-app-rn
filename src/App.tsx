1779b42a4d7f1993185e48fe59153d7f20682464
andychow326 <andychchow326@gmail.com>
2022-07-15 15:46:22 +0800

    Add UserPanelScreen with no-opt

--- src/App.tsx
+++ src/App.tsx
@@ -7,10 +7,12 @@ import {createNativeStackNavigator} from '@react-navigation/native-stack';
 import AuthenticationScreen from './screens/AuthenticationScreen';
 import ConfigurationScreen from './screens/ConfigurationScreen';
 import ConfigProvider from './context/ConfigProvider';
+import UserPanelScreen from './screens/UserPanelScreen';
 
 export type RootStackParamList = {
   Authentication: undefined;
   Configuration: {fromButton: boolean} | undefined;
+  UserPanel: undefined;
 };
 
 const Stack = createNativeStackNavigator<RootStackParamList>();
@@ -23,6 +25,7 @@ const App: React.FC = () => {
         screenOptions={{headerShown: false}}>
         <Stack.Screen name="Authentication" component={AuthenticationScreen} />
         <Stack.Screen name="Configuration" component={ConfigurationScreen} />
+        <Stack.Screen name="UserPanel" component={UserPanelScreen} />
       </Stack.Navigator>
     </ConfigProvider>
   );
---
+++ src/screens/UserPanelScreen.tsx
@@ -0,0 +1,110 @@
+import {NativeStackScreenProps} from '@react-navigation/native-stack';
+import React, {useState} from 'react';
+import {StyleSheet, View} from 'react-native';
+import {
+  Appbar,
+  Button,
+  Card,
+  Dialog,
+  Divider,
+  Portal,
+  Text,
+  useTheme,
+} from 'react-native-paper';
+import {RootStackParamList} from '../App';
+
+const styles = StyleSheet.create({
+  container: {
+    margin: 16,
+  },
+  contentText: {
+    fontSize: 16,
+    fontWeight: '400',
+    lineHeight: 24,
+  },
+
+  userInfoCard: {
+    backgroundColor: '#F5F5F5',
+    marginVertical: 16,
+  },
+
+  buttonContainer: {
+    marginVertical: 8,
+  },
+  button: {
+    flexDirection: 'row',
+    marginVertical: 8,
+  },
+});
+
+type UserPanelScreenProps = NativeStackScreenProps<
+  RootStackParamList,
+  'UserPanel'
+>;
+
+const UserPanelScreen: React.FC<UserPanelScreenProps> = props => {
+  const navigation = props.navigation;
+  const theme = useTheme();
+  const [infoDialogVisble, setInfoDialogVisible] = useState(false);
+
+  return (
+    <>
+      <Appbar.Header>
+        <Appbar.Content title="Authgear Demo" />
+        <Appbar.Action
+          icon="information-outline"
+          onPress={() => setInfoDialogVisible(true)}
+        />
+      </Appbar.Header>
+
+      <Portal>
+        <Dialog visible={infoDialogVisble}>
+          <Dialog.Title>Configuration</Dialog.Title>
+          <Dialog.Content>
+            <Text>Hi</Text>
+          </Dialog.Content>
+          <Dialog.Actions>
+            <Button onPress={() => setInfoDialogVisible(false)}>Dismiss</Button>
+          </Dialog.Actions>
+        </Dialog>
+      </Portal>
+
+      <View style={styles.container}>
+        <Text style={styles.contentText}>Welcome!</Text>
+
+        <Card.Title
+          title="Card Title"
+          subtitle="Card Subtitle"
+          style={styles.userInfoCard}
+        />
+
+        <View style={styles.buttonContainer}>
+          <Button compact={true} uppercase={false} style={styles.button}>
+            <Text style={styles.contentText}>User Information</Text>
+          </Button>
+          <Divider />
+          <Button compact={true} uppercase={false} style={styles.button}>
+            <Text style={styles.contentText}>User Settings</Text>
+          </Button>
+          <Divider />
+          <Button compact={true} uppercase={false} style={styles.button}>
+            <Text style={styles.contentText}>Enable Biometric Login</Text>
+          </Button>
+          <Divider />
+          <Button compact={true} uppercase={false} style={styles.button}>
+            <Text style={styles.contentText}>Reauthenticate</Text>
+          </Button>
+          <Divider />
+          <Button compact={true} uppercase={false} style={styles.button}>
+            <Text style={{...styles.contentText, color: theme.colors.error}}>
+              Logout
+            </Text>
+          </Button>
+          <Divider />
+        </View>
+      </View>
+    </>
+  );
+};
+
+export default UserPanelScreen;
