49f2b6d6f98346fdf878fc1584af8af111555070
andychow326 <andychchow326@gmail.com>
2022-07-20 14:44:44 +0800

    Isolate ShowError function

---
+++ src/ShowError.tsx
@@ -0,0 +1,70 @@
+import {
+  CancelError,
+  BiometricPrivateKeyNotFoundError,
+  BiometricNoEnrollmentError,
+  BiometricNotSupportedOrPermissionDeniedError,
+  BiometricNoPasscodeError,
+  BiometricLockoutError,
+} from '@authgear/react-native';
+import {Platform, Alert} from 'react-native';
+
+const ShowError = (e: any) => {
+  const json = JSON.parse(JSON.stringify(e));
+  delete json.line;
+  delete json.column;
+  delete json.sourceURL;
+  json['constructor.name'] = e?.constructor?.name;
+  json.message = e.message;
+  const title = 'Error';
+  let message = JSON.stringify(json);
+
+  if (e instanceof CancelError) {
+    // Cancel is not an error actually.
+    return;
+  }
+
+  if (e instanceof BiometricPrivateKeyNotFoundError) {
+    message = Platform.select({
+      android:
+        'Your biometric info has changed. For security reason, you have to set up biometric authentication again.',
+      ios: 'Your Touch ID or Face ID has changed. For security reason, you have to set up biometric authentication again.',
+      default: message,
+    });
+  }
+
+  if (e instanceof BiometricNoEnrollmentError) {
+    message = Platform.select({
+      android:
+        'You have not set up biometric yet. Please set up your fingerprint or face',
+      ios: 'You do not have Face ID or Touch ID set up yet. Please set it up first',
+      default: message,
+    });
+  }
+
+  if (e instanceof BiometricNotSupportedOrPermissionDeniedError) {
+    message = Platform.select({
+      android:
+        'Your device does not support biometric. The developer should have checked this and not letting you to see feature that requires biometric',
+      ios: 'If the developer should performed checking, then it is likely that you have denied the permission of Face ID. Please enable it in Settings',
+      default: message,
+    });
+  }
+
+  if (e instanceof BiometricNoPasscodeError) {
+    message = Platform.select({
+      android:
+        'You device does not have credential set up. Please set up either a PIN, a pattern or a password',
+      ios: 'You device does not have passcode set up. Please set up a passcode',
+      default: message,
+    });
+  }
+
+  if (e instanceof BiometricLockoutError) {
+    message =
+      'The biometric is locked out due to too many failed attempts. The developer should handle this error by using normal authentication as a fallback. So normally you should not see this error';
+  }
+
+  Alert.alert(title, message);
+};
+
+export default ShowError;
--- src/screens/AuthenticationScreen.tsx
+++ src/screens/AuthenticationScreen.tsx
@@ -1,20 +1,15 @@
 import React, {useCallback, useEffect, useState} from 'react';
-import {Alert, Platform, StyleSheet, View} from 'react-native';
+import {Platform, StyleSheet, View} from 'react-native';
 import {useTheme, Button, Text, ActivityIndicator} from 'react-native-paper';
 import {NativeStackScreenProps} from '@react-navigation/native-stack';
 import {RootStackParamList} from '../App';
 import authgear, {
-  BiometricLockoutError,
-  BiometricNoEnrollmentError,
-  BiometricNoPasscodeError,
-  BiometricNotSupportedOrPermissionDeniedError,
-  BiometricPrivateKeyNotFoundError,
-  CancelError,
   PersistentTokenStorage,
   TransientTokenStorage,
 } from '@authgear/react-native';
 import {useConfig} from '../context/ConfigProvider';
 import {useUserInfo} from '../context/UserInfoProvider';
+import ShowError from '../ShowError';
 
 const styles = StyleSheet.create({
   loading: {
@@ -66,65 +61,6 @@ const wechatRedirectURI = Platform.select<string>({
   ios: 'https://authgear-demo-rn.pandawork.com/authgear/open_wechat_app',
 });
 
-const showError = (e: any) => {
-  const json = JSON.parse(JSON.stringify(e));
-  delete json.line;
-  delete json.column;
-  delete json.sourceURL;
-  json['constructor.name'] = e?.constructor?.name;
-  json.message = e.message;
-  const title = 'Error';
-  let message = JSON.stringify(json);
-
-  if (e instanceof CancelError) {
-    // Cancel is not an error actually.
-    return;
-  }
-
-  if (e instanceof BiometricPrivateKeyNotFoundError) {
-    message = Platform.select({
-      android:
-        'Your biometric info has changed. For security reason, you have to set up biometric authentication again.',
-      ios: 'Your Touch ID or Face ID has changed. For security reason, you have to set up biometric authentication again.',
-      default: message,
-    });
-  }
-
-  if (e instanceof BiometricNoEnrollmentError) {
-    message = Platform.select({
-      android:
-        'You have not set up biometric yet. Please set up your fingerprint or face',
-      ios: 'You do not have Face ID or Touch ID set up yet. Please set it up first',
-      default: message,
-    });
-  }
-
-  if (e instanceof BiometricNotSupportedOrPermissionDeniedError) {
-    message = Platform.select({
-      android:
-        'Your device does not support biometric. The developer should have checked this and not letting you to see feature that requires biometric',
-      ios: 'If the developer should performed checking, then it is likely that you have denied the permission of Face ID. Please enable it in Settings',
-      default: message,
-    });
-  }
-
-  if (e instanceof BiometricNoPasscodeError) {
-    message = Platform.select({
-      android:
-        'You device does not have credential set up. Please set up either a PIN, a pattern or a password',
-      ios: 'You device does not have passcode set up. Please set up a passcode',
-      default: message,
-    });
-  }
-
-  if (e instanceof BiometricLockoutError) {
-    message =
-      'The biometric is locked out due to too many failed attempts. The developer should handle this error by using normal authentication as a fallback. So normally you should not see this error';
-  }
-
-  Alert.alert(title, message);
-};
-
 type AuthenticationScreenProps = NativeStackScreenProps<
   RootStackParamList,
   'Authentication'
@@ -164,7 +100,7 @@ const AuthenticationScreen: React.FC<AuthenticationScreenProps> = props => {
         shareSessionWithSystemBrowser,
       })
       .catch(e => {
-        showError(e);
+        ShowError(e);
       });
   }, [config, navigation]);
 
@@ -184,10 +120,10 @@ const AuthenticationScreen: React.FC<AuthenticationScreenProps> = props => {
         })
         .then(({userInfo}) => {
           setUserInfo(userInfo);
-          navigation.navigate('UserPanel');
+          navigation.replace('UserPanel');
         })
         .catch(e => {
-          showError(e);
+          ShowError(e);
         })
         .finally(() => {
           setLoading(false);
@@ -210,10 +146,10 @@ const AuthenticationScreen: React.FC<AuthenticationScreenProps> = props => {
       .authenticateAnonymously()
       .then(({userInfo}) => {
         setUserInfo(userInfo);
-        navigation.navigate('UserPanel');
+        navigation.replace('UserPanel');
       })
       .catch(e => {
-        showError(e);
+        ShowError(e);
       })
       .finally(() => {
         setLoading(false);
