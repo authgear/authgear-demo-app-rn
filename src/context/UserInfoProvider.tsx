f1ec7d89fc8441d72019e387d5067787725acf3b
andychow326 <andychchow326@gmail.com>
2022-07-20 13:46:34 +0800

    Introduce UserInfoProvider

---
+++ src/context/UserInfoProvider.tsx
@@ -0,0 +1,30 @@
+import {UserInfo} from '@authgear/react-native';
+import React, {createContext, useContext, useState} from 'react';
+
+interface userInfoContextProviderValue {
+  userInfo: UserInfo | null;
+  setUserInfo: React.Dispatch<React.SetStateAction<UserInfo | null>>;
+}
+
+const userInfoContext = createContext<userInfoContextProviderValue>({
+  userInfo: null,
+  setUserInfo: () => {},
+});
+
+interface UserInfoProviderProps {
+  children: React.ReactNode;
+}
+
+const UserInfoProvider: React.FC<UserInfoProviderProps> = ({children}) => {
+  const [userInfo, setUserInfo] = useState<UserInfo | null>(null);
+
+  return (
+    <userInfoContext.Provider value={{userInfo, setUserInfo}}>
+      {children}
+    </userInfoContext.Provider>
+  );
+};
+
+export const useUserInfo = () => useContext(userInfoContext);
+
+export default UserInfoProvider;
