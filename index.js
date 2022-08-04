1a3e12c20cb42b2bbf1c5479f7b3d29f01bcdd90
andychow326 <andychchow326@gmail.com>
2022-08-04 16:12:06 +0800

    Add dark theme color

--- index.js
+++ index.js
@@ -3,25 +3,52 @@
  */
 
 import React from 'react';
-import { AppRegistry } from 'react-native';
-import { NavigationContainer } from '@react-navigation/native';
-import { DefaultTheme, Provider as PaperProvider } from 'react-native-paper';
+import { AppRegistry, useColorScheme } from 'react-native';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 import { name as appName } from './app.json';
 import App from './src/App';
 
-const theme = {
-  ...DefaultTheme,
-  version: 3,
+const CombinedDefaultTheme = {
+  ...PaperDefaultTheme,
+  ...NavigationDefaultTheme,
   colors: {
-    ...DefaultTheme.colors,
-    background: '#FFFFFF',
+    ...PaperDefaultTheme.colors,
+    ...NavigationDefaultTheme.colors,
+    background: '#F8F8F8',
+    shadedBackground: '#EFEFEF',
     primary: '#0099FF',
     disabled: 'rgba(0, 0, 0, 0.6)',
     error: '#B00020',
   },
 };
+const CombinedDarkTheme = {
+  ...PaperDarkTheme,
+  ...NavigationDarkTheme,
+  colors: {
+    ...PaperDarkTheme.colors,
+    ...NavigationDarkTheme.colors,
+    background: '#212121',
+    shadedBackground: '#101010',
+    primary: '#0099FF',
+    disabled: 'rgba(255, 255, 255, 0.6)',
+    error: '#B00020',
+  },
+};
 
 export default function Main() {
+  const systemColorScheme = useColorScheme();
+  const theme =
+    systemColorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme;
+
   return (
     <PaperProvider theme={theme}>
       <NavigationContainer theme={theme}>
