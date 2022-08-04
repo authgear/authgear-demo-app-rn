8418855ffb58f7821e7e85381f7f128c73fe7bd1
andychow326 <andychchow326@gmail.com>
2022-08-04 16:29:22 +0800

    Move PaperProvider and NavigationContainer from index.js to App.tsx

--- index.js
+++ index.js
@@ -3,59 +3,12 @@
  */
 
 import React from 'react';
-import { AppRegistry, useColorScheme } from 'react-native';
-import {
-  NavigationContainer,
-  DarkTheme as NavigationDarkTheme,
-  DefaultTheme as NavigationDefaultTheme,
-} from '@react-navigation/native';
-import {
-  DarkTheme as PaperDarkTheme,
-  DefaultTheme as PaperDefaultTheme,
-  Provider as PaperProvider,
-} from 'react-native-paper';
+import { AppRegistry } from 'react-native';
 import { name as appName } from './app.json';
 import App from './src/App';
 
-const CombinedDefaultTheme = {
-  ...PaperDefaultTheme,
-  ...NavigationDefaultTheme,
-  colors: {
-    ...PaperDefaultTheme.colors,
-    ...NavigationDefaultTheme.colors,
-    background: '#F8F8F8',
-    shadedBackground: '#EFEFEF',
-    primary: '#0099FF',
-    disabled: 'rgba(0, 0, 0, 0.6)',
-    error: '#B00020',
-  },
-};
-const CombinedDarkTheme = {
-  ...PaperDarkTheme,
-  ...NavigationDarkTheme,
-  colors: {
-    ...PaperDarkTheme.colors,
-    ...NavigationDarkTheme.colors,
-    background: '#212121',
-    shadedBackground: '#101010',
-    primary: '#0099FF',
-    disabled: 'rgba(255, 255, 255, 0.6)',
-    error: '#B00020',
-  },
-};
-
 export default function Main() {
-  const systemColorScheme = useColorScheme();
-  const theme =
-    systemColorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme;
-
-  return (
-    <PaperProvider theme={theme}>
-      <NavigationContainer theme={theme}>
-        <App />
-      </NavigationContainer>
-    </PaperProvider>
-  );
+  return <App />;
 }
 
 AppRegistry.registerComponent(appName, () => Main);
--- src/App.tsx
+++ src/App.tsx
@@ -3,13 +3,23 @@
  */
 
 import React from 'react';
+import {
+  NavigationContainer,
+  DarkTheme as NavigationDarkTheme,
+  DefaultTheme as NavigationDefaultTheme,
+} from '@react-navigation/native';
+import {
+  DarkTheme as PaperDarkTheme,
+  DefaultTheme as PaperDefaultTheme,
+  Provider as PaperProvider,
+} from 'react-native-paper';
 import { createNativeStackNavigator } from '@react-navigation/native-stack';
 import AuthenticationScreen from './screens/AuthenticationScreen';
 import ConfigurationScreen from './screens/ConfigurationScreen';
 import ConfigProvider from './context/ConfigProvider';
 import UserPanelScreen from './screens/UserPanelScreen';
 import UserInfoScreen from './screens/UserInfoScreen';
-import { Platform } from 'react-native';
+import { Platform, useColorScheme } from 'react-native';
 import UserProvider from './context/UserProvider';
 import { UserInfo } from '@authgear/react-native';
 
@@ -43,22 +53,60 @@ export const biometricOptions = {
   },
 };
 
+const CombinedDefaultTheme = {
+  ...PaperDefaultTheme,
+  ...NavigationDefaultTheme,
+  colors: {
+    ...PaperDefaultTheme.colors,
+    ...NavigationDefaultTheme.colors,
+    background: '#F8F8F8',
+    shadedBackground: '#EFEFEF',
+    primary: '#0099FF',
+    disabled: 'rgba(0, 0, 0, 0.6)',
+    error: '#B00020',
+  },
+};
+const CombinedDarkTheme = {
+  ...PaperDarkTheme,
+  ...NavigationDarkTheme,
+  colors: {
+    ...PaperDarkTheme.colors,
+    ...NavigationDarkTheme.colors,
+    background: '#212121',
+    shadedBackground: '#101010',
+    primary: '#0099FF',
+    disabled: 'rgba(255, 255, 255, 0.6)',
+    error: '#B00020',
+  },
+};
+
 const App: React.FC = () => {
+  const systemColorScheme = useColorScheme();
+  const theme =
+    systemColorScheme === 'dark' ? CombinedDarkTheme : CombinedDefaultTheme;
+
   return (
     <ConfigProvider>
       <UserProvider>
-        <Stack.Navigator
-          initialRouteName="Authentication"
-          screenOptions={{ headerShown: false }}
-        >
-          <Stack.Screen
-            name="Authentication"
-            component={AuthenticationScreen}
-          />
-          <Stack.Screen name="Configuration" component={ConfigurationScreen} />
-          <Stack.Screen name="UserPanel" component={UserPanelScreen} />
-          <Stack.Screen name="UserInfo" component={UserInfoScreen} />
-        </Stack.Navigator>
+        <PaperProvider theme={theme}>
+          <NavigationContainer theme={theme}>
+            <Stack.Navigator
+              initialRouteName="Authentication"
+              screenOptions={{ headerShown: false }}
+            >
+              <Stack.Screen
+                name="Authentication"
+                component={AuthenticationScreen}
+              />
+              <Stack.Screen
+                name="Configuration"
+                component={ConfigurationScreen}
+              />
+              <Stack.Screen name="UserPanel" component={UserPanelScreen} />
+              <Stack.Screen name="UserInfo" component={UserInfoScreen} />
+            </Stack.Navigator>
+          </NavigationContainer>
+        </PaperProvider>
       </UserProvider>
     </ConfigProvider>
   );
