ffe44bcd94e7c6a2dc6c35fa2988d3d11b368a8b
Louis Chan <louischan@oursky.com>
2025-11-24 14:08:11 +0800

    Update React Native to 0.79.7

--- ios/AuthgearDemoAppRN/AppDelegate.h
+++ ios/AuthgearDemoAppRN/AppDelegate.h
@@ -1,6 +1,14 @@
-#import <RCTAppDelegate.h>
 #import <UIKit/UIKit.h>
+#import <RCTReactNativeFactory.h>
+#import <RCTDefaultReactNativeFactoryDelegate.h>
 
-@interface AppDelegate : RCTAppDelegate
+@interface ReactNativeDelegate : RCTDefaultReactNativeFactoryDelegate
+@end
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (nonatomic, strong, nonnull) UIWindow *window;
+@property (nonatomic, strong, nonnull) RCTReactNativeFactory *reactNativeFactory;
+@property (nonatomic, strong, nonnull) ReactNativeDelegate *reactNativeDelegate;
 
 @end
