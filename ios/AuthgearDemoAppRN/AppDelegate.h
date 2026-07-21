554a1ffbd2d2ae66e95f8a91319fe7f59c9887d7
fungc-io <fung@oursky.com>
2026-07-21 15:41:34 +0100

    chore: add react-native-app-auth and OIDC redirect scheme

--- ios/AuthgearDemoAppRN/AppDelegate.h
+++ ios/AuthgearDemoAppRN/AppDelegate.h
@@ -1,14 +1,16 @@
 #import <UIKit/UIKit.h>
 #import <RCTReactNativeFactory.h>
 #import <RCTDefaultReactNativeFactoryDelegate.h>
+#import "RNAppAuthAuthorizationFlowManager.h"
 
 @interface ReactNativeDelegate : RCTDefaultReactNativeFactoryDelegate
 @end
 
-@interface AppDelegate : UIResponder <UIApplicationDelegate>
+@interface AppDelegate : UIResponder <UIApplicationDelegate, RNAppAuthAuthorizationFlowManager>
 
 @property (nonatomic, strong, nonnull) UIWindow *window;
 @property (nonatomic, strong, nonnull) RCTReactNativeFactory *reactNativeFactory;
 @property (nonatomic, strong, nonnull) ReactNativeDelegate *reactNativeDelegate;
+@property (nonatomic, weak, nullable) id<RNAppAuthAuthorizationFlowManagerDelegate> authorizationFlowManagerDelegate;
 
 @end
